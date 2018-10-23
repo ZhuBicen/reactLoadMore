dd5ab1c3f35b9ae47afb0c1375c108a4dfe9cdc8
AB <ab@chromium.org>
2018-10-23 21:20:51 +0800

    try scroller

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import WeUI from 'react-weui'
-import 'weui';
-import 'react-weui/build/packages/react-weui.css';
-const {
-    InfiniteLoader,
-    Cells,
-    CellsTitle,
-    Cell,
-    CellBody,
-    CellFooter,
-    Page
-} = WeUI
+import InfiniteScroll from 'react-infinite-scroller';
+
 class App extends Component {
     constructor(props) {
         super(props)
@@ -21,50 +11,23 @@ class App extends Component {
         }
     }
     render() {
+        console.log("size:", this.state.items.length);
         return (
-            <InfiniteLoader
-                onLoadMore={(resolve, finish) => {
-                    console.log("onLoadMore");
-                    //mock request
-                    setTimeout(() => {
-                        if (this.state.items.length > 22) {
-                            finish()
-                        } else {
-                            this.setState({
-                                items: this.state.items.concat([this.state.items.length])
-                            }, () => resolve())
-                        }
-                    }, 1000)
-                }}
+            <InfiniteScroll
+                pageStart={0}
+                loadMore={() => { this.setState({items: [...this.state.items, this.state.items.length + 1]}) }}
+                hasMore={this.state.items.length < 100}
+                loader={<div className="loader" > Loading ...</div >}>
+                <ul>
+                    {
+                        this.state.items.map(e => {
+                            return <li key={e}>{e}</li>
+                        })
 
-                onScroll= {()=> {
-                    console.log('onScroll');
-                }}
+                    }
+                </ul>
 
-                onScrollEnd={ ()=> {
-                    console.log('onScrollEnd');
-                }}
-            >
-                <Page className="infinite" title="Infinite Loader" subTitle="滚动加载" >
-
-                    <CellsTitle>List with 22 Max</CellsTitle>
-                    <Cells>
-                        {
-                            this.state.items.map((item, i) => {
-                                return (
-                                    <Cell href="javascript:;" key={i} access>
-                                        <CellBody>
-                                            {item}
-                                        </CellBody>
-                                        <CellFooter />
-                                    </Cell>
-                                )
-                            })
-                        }
-                    </Cells>
-
-                </Page>
-            </InfiniteLoader>
+            </InfiniteScroll >
         )
     }
 }
